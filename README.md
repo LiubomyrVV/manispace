38fe028a9b23ad8ba0461966f541182311e8b359
liubomyr <liubomyrpoland@gmail.com>
2025-07-29 21:50:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+07.29.2025 - Started Project
+
+Task 1:
+Finish project configuration.
+Set up Tailwind styles.
+#NOTE: Read more deeply about Tailwind configuration.
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center">Welcome to Manispace</h1>
+        <p>Your space for managing tasks and projects.</p>
+        <button className="btn-primary">Hello</button>
+      </header>
+    </div>
+  )
+}
+
+export default App
