b741cf89256f3a2c84831e6b051e836068bdd843
liubomyr <liubomyrpoland@gmail.com>
2025-12-29 15:57:39 +0100

    tailwind update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 07.29.2025 - Started Project
 
 Task 1:
-Finish project configuration.
-Set up Tailwind styles.
-#NOTE: Read more deeply about Tailwind configuration.
+ • Finish project configuration.
+ ✔ Set up Tailwind styles.
+#NOTE: Read more deeply about Tailwind configuration. 
+
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-center">Welcome to Manispace</h1>
-        <p>Your space for managing tasks and projects.</p>
+        <h1 className="text-center bg-primary">Welcome to Manispace</h1>
+        <p className="text-mint-500">Your space for managing tasks and projects.</p>
         <button className="btn-primary">Hello</button>
       </header>
     </div>
